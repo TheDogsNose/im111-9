910b9709ea1ca768d0c67632fb2b7867e42abeb2
TheDogsNose <94315145+TheDogsNose@users.noreply.github.com>
2021-11-28 14:36:36 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+var all_answers = "2141111";
+var all_maxAnswers = "4444444";
+var all_instruction = "1111111";
+var all_explain = "0000000";
+var all_choice = "9999999";
+var currentQuestion = 0;
+reference = new Array("0","0","0","0","0","0","0");
